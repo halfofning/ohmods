82334c90176fa356e8b05a565deddb0e192ce080
Ning <wanninglim@gmail.com>
2018-09-26 18:30:52 +0800

    Add text alignment

--- js/storage.js
+++ js/storage.js
@@ -182,61 +182,61 @@ var year1Modules = [
   {
     name: "Fundamentals of IT Professionals I",
     description: "A broad introduction to the field of IT by exploring the roles, professional practice, ethical expectations and career development paths of IT professionals.",
-    image: "images/fp1.svg",
+    image: "images/year1/fp1.svg",
     tags: ["personaldev"]
   },
   {
     name: "Computing Mathematics",
     description: "This module introduces the basic concepts of relations and functions, matrices, statistical methods and relevant applications.",
-    image: "images/cm.svg",
+    image: "images/year1/cm.svg",
     tags: [""]
   },
   {
     name: "Enterprise Information Systems",
     description: "This module introduces students to the different components that build up an enterprise information system.",
-    image: "images/eis.svg",
+    image: "images/year1/eis.svg",
     tags: ["businessIT"]
   },
   {
     name: "Cyber Security Fundamentals",
     description: "This module provides an overview of the domains of cyber security to help develop an understanding of its importance in today’s digital world.",
-    image: "images/csf.svg",
+    image: "images/year1/csf.svg",
     tags: ["infoSecurity"]
   },
   {
     name: "Programming I",
     description: "This module introduces the fundamentals of programming and how to develop programs using appropriate problem solving techniques in a modular style.",
-    image: "images/prg1.svg",
+    image: "images/year1/prg1.svg",
     tags: [""]
   },
   {
     name: "Databases",
     description: "This module introduces students to the underlying concepts of database systems and how to model and design database systems that reflect business requirements.",
-    image: "images/db.svg",
+    image: "images/year1/db.svg",
     tags: [""]
   },
   {
     name: "Front End Development",
     description: "This module teaches the fundamental skills required to develop responsive websites that are optimised for mobile and desktop viewing.",
-    image: "images/fed.svg",
+    image: "images/year1/fed.svg",
     tags: [""]
   },
   {
     name: "Operating Systems and Networking Fundamentals",
     description: "This module focuses on the fundamentals and principles of operating systems and explains what they do.",
-    image: "images/osfnf.svg",
+    image: "images/year1/osfnf.svg",
     tags: [""]
   },
   {
     name: "Portfolio I",
     description: "This module provides the opportunity to apply the knowledge and skills gained from the various modules in the course to date, and explore topics in IT that they have a personal interest.",
-    image: "images/p1.svg",
+    image: "images/year1/p1.svg",
     tags: [""]
   },
   {
     name: "Programming II",
     description: "This module provides opportunities for the students to develop medium-scale applications based on the Object-Oriented (OO) approach.",
-    image: "images/prg2.svg",
+    image: "images/year1/prg2.svg",
     tags: [""]
   },
 ];
@@ -246,43 +246,43 @@ var year2Modules = [
   {
     name: "Fundamentals of IT Professionals II",
     description: "This module gives a course-based experience in which students can engage with the local community and industry.",
-    image: "images/fp2.svg",
+    image: "images/year2/fp2.svg",
     tags: [""]
   },
   {
     name: "Object Oriented Analysis and Design",
     description: "This module leverages the skills acquired in PRG2 to introduce software design, so that students experience the full cycle of software development.",
-    image: "images/ooad.svg",
+    image: "images/year2/ooad.svg",
     tags: [""]
   },
   {
     name: "Web Application Development",
     description: "This module provides students with the knowledge and skills needed to develop web applications and web application protocol interface (API).",
-    image: "images/web.svg",
+    image: "images/year2/web.svg",
     tags: [""]
   },
   {
     name: "Fundamentals of IT Professionals III",
     description: "This module provides a stepping stone to the students in their IT career. They are given an insight into the infocomm industries.",
-    image: "images/fp3.svg",
+    image: "images/year2/fp3.svg",
     tags: [""]
   },
   {
     name: "Full Stack Development",
     description: "This module aims to provide opportunities for students to be part of a software development team working on both back-end and front-end technologies.",
-    image: "images/fsd.svg",
+    image: "images/year2/fsd.svg",
     tags: [""]
   },
   {
     name: "Portfolio II",
     description: "This module builds on the previous module Portfolio I. Students may choose to undertake a real life IT project, a competition-based project or a research and development project.",
-    image: "images/p2.svg",
+    image: "images/year2/p2.svg",
     tags: [""]
   },
   {
     name: "User Experience",
     description: "This module focuses on the principles and techniques for designing good user experience in software applications and other products such as ATMs, kiosks, etc.",
-    image: "images/ux.svg",
+    image: "images/year2/ux.svg",
     tags: [""]
   },
 ]
@@ -519,37 +519,105 @@ function getModules(tags) {
 // Returns the module information that contain the relevant tags.
 function displayYear1Modules() {
     for(var i = 0; i < year1Modules.length; i++) {
-        var img = new Image();
-        img.src = backgrounds[i].image;
-        img.setAttribute("class", "module-bg");
-        document.getElementById('year1-content').appendChild(img);
+        var node = document.createElement("p");
+        var textnode = document.createTextNode(year1Modules[i].name);
+        node.appendChild(textnode);
+        node.setAttribute("class", "module-name");
+
+        var moduleNameDiv = document.createElement("div"); // div containing module name
+        moduleNameDiv.appendChild(node);
+        moduleNameDiv.setAttribute("class", "module-name-div");
+        // document.getElementById('year1-content').appendChild(moduleNameDiv);
 
         var img1 = new Image();
         img1.src = year1Modules[i].image;
         img1.setAttribute("class", "module-img");
-        document.getElementById('year1-content').appendChild(img1);
+
+        var moduleFrontContentDiv = document.createElement("div");
+        moduleFrontContentDiv.appendChild(img1);
+        moduleFrontContentDiv.appendChild(moduleNameDiv);
+        moduleFrontContentDiv.setAttribute("class", "module-front-content-div");
+        document.getElementById('year1-content').appendChild(moduleFrontContentDiv);
+
+        var img = new Image();
+        img.src = backgrounds[i].image;
+        img.setAttribute("class", "module-bg");
+
+        // div containing each modules
+        var moduleDiv = document.createElement("div");
+        moduleDiv.setAttribute("class", "module-div");
+        moduleDiv.appendChild(moduleFrontContentDiv);
+        moduleDiv.appendChild(img)
+        document.getElementById('year1-content').appendChild(moduleDiv);
     }
 }
 
 function displayYear2Modules() {
   for(var i = 0; i < year2Modules.length; i++) {
-      var img = new Image();
-      img.src = backgrounds[i].image;
-      img.setAttribute("class", "module-bg");
-      document.getElementById('year2-content').appendChild(img);
+      var node = document.createElement("p");
+      var textnode = document.createTextNode(year2Modules[i].name);
+      node.appendChild(textnode);
+      node.setAttribute("class", "module-name");
+
+      var moduleNameDiv = document.createElement("div"); // div containing module name
+      moduleNameDiv.appendChild(node);
+      moduleNameDiv.setAttribute("class", "module-name-div");
+      // document.getElementById('year1-content').appendChild(moduleNameDiv);
 
       var img1 = new Image();
       img1.src = year2Modules[i].image;
       img1.setAttribute("class", "module-img");
-      document.getElementById('year2-content').appendChild(img1);
+
+      var moduleFrontContentDiv = document.createElement("div");
+      moduleFrontContentDiv.appendChild(img1);
+      moduleFrontContentDiv.appendChild(moduleNameDiv);
+      moduleFrontContentDiv.setAttribute("class", "module-front-content-div");
+      document.getElementById('year2-content').appendChild(moduleFrontContentDiv);
+
+      var img = new Image();
+      img.src = backgrounds[i].image;
+      img.setAttribute("class", "module-bg");
+
+      // div containing each modules
+      var moduleDiv = document.createElement("div");
+      moduleDiv.setAttribute("class", "module-div");
+      moduleDiv.appendChild(moduleFrontContentDiv);
+      moduleDiv.appendChild(img)
+      document.getElementById('year2-content').appendChild(moduleDiv);
   }
 }
 
 function displayYear3Modules() {
   for(var i = 0; i < year3Modules.length; i++) {
+      var node = document.createElement("p");
+      var textnode = document.createTextNode(year3Modules[i].name);
+      node.appendChild(textnode);
+      node.setAttribute("class", "module-name");
+
+      var moduleNameDiv = document.createElement("div"); // div containing module name
+      moduleNameDiv.appendChild(node);
+      moduleNameDiv.setAttribute("class", "module-name-div");
+      // document.getElementById('year1-content').appendChild(moduleNameDiv);
+
+      var img1 = new Image();
+      img1.src = year3Modules[i].image;
+      img1.setAttribute("class", "module-img");
+
+      var moduleFrontContentDiv = document.createElement("div");
+      moduleFrontContentDiv.appendChild(img1);
+      moduleFrontContentDiv.appendChild(moduleNameDiv);
+      moduleFrontContentDiv.setAttribute("class", "module-front-content-div");
+      document.getElementById('year1-content').appendChild(moduleFrontContentDiv);
+
       var img = new Image();
       img.src = backgrounds[i].image;
       img.setAttribute("class", "module-bg");
-      document.getElementById('year3-content').appendChild(img);
+
+      // div containing each modules
+      var moduleDiv = document.createElement("div");
+      moduleDiv.setAttribute("class", "module-div");
+      moduleDiv.appendChild(moduleFrontContentDiv);
+      moduleDiv.appendChild(img)
+      document.getElementById('year3-content').appendChild(moduleDiv);
   }
 }
